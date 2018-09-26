4696a1a63cdb0b293108eb8cc640eabc858f2d57
Aneesa Saleh <aneesa.saleh@gmail.com>
2018-09-26 15:38:01 +0100

    feat: Create /restaurants/new and /restaurants/new/create routes to add new restaurants

--- webserver.py
+++ webserver.py
@@ -1,5 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import cgi
+from urllib.parse import parse_qs
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -30,6 +30,9 @@ class WebServerHandler(BaseHTTPRequestHandler):
                     </head>
                     <body style="font-family: 'Open Sans', sans-serif; font-weight: 300; padding: 10px 20px;">
                         <h1>Restaurants</h1>
+                        <a style="color: #0083a8; margin: 5px; font-weight: 400;" href="/restaurants/new">
+                            Add new restaurant
+                        </a>
                         <ul style="padding: 0;">
                             {restaurants}
                         </ul>
@@ -60,36 +63,59 @@ class WebServerHandler(BaseHTTPRequestHandler):
             )
             self.wfile.write(page_html.format(restaurants=restaurants_html).encode())
             return
+        if self.path.endswith('/restaurants/new'):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            page_html = '''
+                <!DOCTYPE html>
+                <html lang="en">
+                    <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1">
+                        <title>Restaurants</title>
+                        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400" rel="stylesheet">
+                    </head>
+                    <body style="font-family: 'Open Sans', sans-serif; font-weight: 300; padding: 10px 20px;">
+                        <h1>New Restaurant</h1>
+                        <form method="POST" action="/restaurants/new/create">
+                            <input type="text" name="restaurant_name">
+                            <button type="submit">Create</button>
+                        </form>
+                        <a style="color: #0083a8; margin: 5px; font-weight: 400;" href="/restaurants">
+                            Home
+                        </a>
+                    </body>
+                </html>
+            '''
+            self.wfile.write(page_html.encode())
+            return
         else:
             self.send_error(404, 'File not found: %s' % self.path)
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.send_header('Content-type', 'text/html')
-            self.end_headers()
+            if(self.path.endswith('/restaurants/new/create')):
 
-            ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
-            if ctype == 'multipart/form-data':
-                fields = cgi.parse_multipart(self.rfile, pdict)
-                message_content = fields.get('message')
-                message = '''
-                    <!DOCTYPE html>
-                    <html>
-                        <body>
-                            <h2>Okay, how about this:</h2>
-                            <h1>{message}</h1>
-                            <form method="POST" enctype="multipart/form-data" action="/hello"
-                                <h2>What would you like me to day?</h2>
-                                <input name="message" type="text">
-                                <button type="submit">Submit</button>
-                            </form>
-                        </body>
-                    </html>
-                '''.format(message=message_content)
+                length = int(self.headers.get('Content-length', 0))
+                body = self.rfile.read(length).decode()
+                params = parse_qs(body)
+                if ('restaurant_name' in params):
+                    restaurant_name = params['restaurant_name'][0]
+                    session = create_db_session()
 
-            self.wfile.write(message.encode())
-            print(message)
+                    new_restaurant = Restaurant(name = restaurant_name)
+                    session.add(new_restaurant)
+                    session.commit()
+
+                    self.send_response(303)
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
+                else:
+                    self.send_response(303)
+                    self.send_header('Location', '/restaurants')
+                    self.end_headers()
             return
         except:
             pass
